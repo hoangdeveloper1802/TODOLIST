6e29a5ca77a2bc417673ba94d05345dde2a7e08d
Lê Huy Hoàng <hoangdeveloper1802@gmail.com>
2023-07-28 09:09:50 -0700

    v2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+  const [work, setWork] = useState('')
+
   return (
-    <div className="App">
-     <h1>Hello React</h1>
+    <div className="flex gap-8 h-screen justify-center items-center border border-red-500 ">
+       <input
+       type='text'
+       className='border border-blue-600 outline-none px-2 py-2 w-[400px]'
+       value={work}
+       onChange={e=>setWork(e.target.value)}
+       />
+       <button
+       type='button'
+       className='outline-none bg-blue-700 px-4 py-2 rounded-md text-white'
+       >Add</button>
     </div>
   );
 }
