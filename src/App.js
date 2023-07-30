5747cf1054d0138945609205f8bc79c733f1076a
Lê Huy Hoàng <hoangdeveloper1802@gmail.com>
2023-07-30 00:15:22 -0700

    v4

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 
 import './App.css';
 import { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 function App() {
@@ -8,12 +10,21 @@ function App() {
   const [todos, setTodos] = useState([]);
 
   const handleAdd = ()=>{
-    setTodos(prev=>[work, ...prev])
-    setWork('')
+    if(todos.some(item=>item===work)){
+      toast.warn('Công việc đã được thêm vào')
+    }else{
+      setTodos(prev=>[work, ...prev])
+      setWork('')
+    }
+  }
+
+  const handleDelete = (index)=>{
+    setTodos(todos.splice(index+1,1));
   }
 
   return (
-    <div className="flex flex-col gap-8 h-screen justify-center items-center border border-red-500 ">
+    <>
+        <div className="flex flex-col gap-8 h-screen justify-center items-center border border-red-500 ">
        <div className="flex gap-8">
           <input
           type='text'
@@ -30,10 +41,34 @@ function App() {
        <div>
           <h3 className='font-bold text-2xl'>Content</h3>
           <ul>
-            {todos.map((todo, index)=><li key={index}>{todo}</li>)}
+            {todos.map((todo, index)=>
+            <li key={index}>
+              <span>{todo}</span>
+              <button
+              type='button'
+              className='outline-none bg-red-700 px-4 py-2 ml-2 rounded-md text-white'
+              onClick={()=>handleDelete(index)}
+              >X</button>
+            </li>
+            )}
           </ul>
        </div>
     </div>
+      <ToastContainer
+    position="top-right"
+    autoClose={5000}
+    hideProgressBar={false}
+    newestOnTop={false}
+    closeOnClick
+    rtl={false}
+    pauseOnFocusLoss
+    draggable
+    pauseOnHover
+    theme="light"
+    />
+    {/* Same as */}
+    <ToastContainer />
+    </>
   );
 }
 
